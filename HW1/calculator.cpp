d0047f537e5a140cc0355f6275956168ebfab45c
fatepattern <56046134+fatepattern@users.noreply.github.com>
2020-12-22 18:34:50 +0200

    Add files via upload

--- HW1/calculator.cpp
+++ HW1/calculator.cpp
@@ -9,3 +9,12 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+//.randomizer_page {
+    //background: none;
+    //border: 2px solid #9FFF00;
+    //border-radius: 100px;
+    //color: #9FFF00;
+    //text-align: center;
+    //margin-left: 35%;
+    //margin-right: 35%;
+//}
